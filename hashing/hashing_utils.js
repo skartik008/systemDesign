ac9ec9d28269619ab2f52a0f769567ba12143160
Kartik Siddhabathula <ksiddhabathula@apple.com>
2020-03-15 12:24:32 -0700

    Program to demonstrate hashing and assigning users to servers

--- hashing/hashing_utils.js
+++ hashing/hashing_utils.js
@@ -1,3 +1,6 @@
+
+// This is a simple hashing function, sourced from stackoverflow.
+
 function hashString(string){
   let hash = 0;
   if(string.length === 0) return hash;
@@ -8,6 +11,13 @@ function hashString(string){
   }
   return hash;
 }
+
+/*
+This is used in Rendezvous hashing, it computes a score for the
+given username and the server, based on mathematical computation
+using prime numbers. The values have been picked randomly and tested
+until the given values work fine.
+*/
 
 function computeScore(username, server) {
   const userNameHash = hashString(username);
