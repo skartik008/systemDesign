a1c7b03cbb84e9c0513ceff767d81126a9d48d9f
Kartik Siddhabathula <ksiddhabathula@apple.com>
2020-03-14 20:36:46 -0700

    Program to demonstrate how proxy works

--- proxy/server.js
+++ proxy/server.js
@@ -12,5 +12,5 @@ app.listen(port, () => console.log('Listening on port '+ port +'.'));
 
 app.get('/hello',(req,res) => {
 	console.log('Headers:', req.headers);
-	res.send('Hello!\n');
+	res.send('Hello this is demo for proxy!\n');
 });
